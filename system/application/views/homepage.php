549df6c56196b526208f76c263dbbbeab6459d96
Phil Dokas <phil@jetless.org>
2009-12-20 14:10:49 -0500

    Adding in Markdown and SmartyPants filtering

--- system/application/views/homepage.php
+++ system/application/views/homepage.php
@@ -4,11 +4,11 @@
 	<time datetime="<?php echo format_sql_datetime($entry->datetime); ?>" pubdate><?php echo format_sql_datetime($entry->datetime, 'M j, ~ga'); ?></time>
 
 	<?php if ($entry->excerpt): ?>
-		<?php echo $entry->excerpt; ?>
+		<?php echo smartypants(markdown($entry->excerpt)); ?>
 		
 		<a href='#'>More &rarr;</a>
 	<?php else: ?>
-		<?php echo $entry->body; ?>
+		<?php echo smartypants(markdown($entry->body)); ?>
 	<?php endif; ?>
 </article>
 <?php endforeach; ?>
