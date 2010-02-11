7eb4611890cc28f9aeb88792a0996c5dbbe26e05
Phil Dokas <phil@jetless.org>
2010-02-10 22:47:49 -0500

    Updating JS assets

--- system/application/views/includes/footer.php
+++ system/application/views/includes/footer.php
@@ -4,7 +4,7 @@
 		</footer>
 	</div>
 
-	<script type='text/javascript' src='/assets/j/jquery-1.3.2.min.js'></script>
-	<script type='text/javascript' src='/assets/j/modernizr-1.0.min.js'></script>
+	<script type='text/javascript' src='/assets/j/jquery-1.4.1.min.js'></script>
+	<script type='text/javascript' src='/assets/j/modernizr-1.1.min.js'></script>
 </body>
 </html>
