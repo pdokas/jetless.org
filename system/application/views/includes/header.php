4441e9efa043c54ce197ac76e7ac33e849cb3520
Phil Dokas <phil@jetless.org>
2010-08-31 23:10:20 -0400

    Updated header fonts

--- system/application/views/includes/header.php
+++ system/application/views/includes/header.php
@@ -4,13 +4,28 @@
 	<meta charset='utf-8'>
 	<title><?php echo (isset($title) ? $title.' | ' : '') ?>jetless heights</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="/assets/c/master-admin.css">
+	<script type="text/javascript">
+		WebFontConfig = {
+			typekit: { id: 'mmr7kyz' }
+		};
+		(function() {
+			var wf = document.createElement('script');
+			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+			'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+			wf.type = 'text/javascript';
+			wf.async = 'true';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wf, s);
+		})();
+	</script>
 </head>
 
 <body>
 	<div id='wrapper'>
 		<header>
 			<hgroup>
-				<h1><a href='/'>jetless</a></h1>
+				<h6>I dreamt of</h6>
+				<h1><a href='/'>Jetless Heights</a></h1>
 			</hgroup>
 		</header>
 	
