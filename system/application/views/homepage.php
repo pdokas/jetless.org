eb681e41c29b3ab2cb62e73288a14e19aeb7e0c1
Phil Dokas <phil@jetless.org>
2010-09-03 00:47:18 -0400

    Changing fonts around

--- system/application/views/homepage.php
+++ system/application/views/homepage.php
@@ -1,14 +1,16 @@
+<div id='articles'>
 <?php foreach ($entries as $entry): ?>
-<article>
-	<h1><?php echo smartypants($entry->title); ?></h1>
-	<time datetime="<?php echo format_sql_datetime($entry->datetime); ?>" pubdate><?php echo format_sql_datetime($entry->datetime, 'M j, ~ga'); ?></time>
+	<article>
+		<h1><?php echo smartypants($entry->title); ?></h1>
+		<time datetime="<?php echo format_sql_datetime($entry->datetime); ?>" pubdate><?php echo format_sql_datetime($entry->datetime, 'M j, ~ga'); ?></time>
 
-	<?php if ($entry->excerpt): ?>
-		<?php echo typeset($entry->excerpt); ?>
+		<?php if ($entry->excerpt): ?>
+			<?php echo typeset($entry->excerpt); ?>
 		
-		<a href='#'>More &rarr;</a>
-	<?php else: ?>
-		<?php echo typeset($entry->body); ?>
-	<?php endif; ?>
-</article>
+			<a href='#'>More &rarr;</a>
+		<?php else: ?>
+			<?php echo typeset($entry->body); ?>
+		<?php endif; ?>
+	</article>
 <?php endforeach; ?>
+</div>
