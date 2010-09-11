a8aee781a4740525afe1e4758d34acf12b9e7318
Phil Dokas <phil@jetless.org>
2010-09-11 13:51:36 -0400

    Updating libraries, further refined styling

--- system/application/views/includes/footer.php
+++ system/application/views/includes/footer.php
@@ -4,7 +4,21 @@
 		</footer>
 	</div>
 
-	<script type='text/javascript' src='/assets/j/jquery-1.4.1.min.js'></script>
-	<script type='text/javascript' src='/assets/j/modernizr-1.1.min.js'></script>
+	
+	<script type="text/javascript">
+		WebFontConfig = {
+			typekit: { id: 'mmr7kyz' }
+		};
+		(function() {
+			var wf = document.createElement('script');
+			wf.src = 'http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+			wf.type = 'text/javascript';
+			wf.async = 'true';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wf, s);
+		})();
+	</script>
+	<script type='text/javascript' src='/assets/j/jquery-1.4.2.min.js'></script>
+	<script type='text/javascript' src='/assets/j/modernizr-1.5.min.js'></script>
 </body>
 </html>
--- system/application/views/includes/header.php
+++ system/application/views/includes/header.php
@@ -4,20 +4,6 @@
 	<meta charset='utf-8'>
 	<title><?php echo (isset($title) ? $title.' | ' : '') ?>jetless heights</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="/assets/c/master-admin.css">
-	<script type="text/javascript">
-		WebFontConfig = {
-			typekit: { id: 'mmr7kyz' }
-		};
-		(function() {
-			var wf = document.createElement('script');
-			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-			'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-			wf.type = 'text/javascript';
-			wf.async = 'true';
-			var s = document.getElementsByTagName('script')[0];
-			s.parentNode.insertBefore(wf, s);
-		})();
-	</script>
 </head>
 
 <body>
